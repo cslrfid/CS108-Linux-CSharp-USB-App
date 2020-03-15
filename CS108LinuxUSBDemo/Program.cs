8eef63648df1f273ea5c953a11cb765cc46fadd6
Carlson Lam <4546682+ksclam@users.noreply.github.com>
2020-03-15 00:59:49 -0400

    initial commit

---
+++ CS108LinuxUSBDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CS108LinuxUSBDemo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string deviceString = "";
+
+            try
+            {
+                // Iterate through each HID device with matching VID/PID
+                for (int i = 0; i < HID.GetNumHidDevices(); i++)
+                {
+                    // Add path strings to the combobox
+                    if (HID.GetHidString(i, ref deviceString) == HID.HID_DEVICE_SUCCESS)
+                    {
+                        Console.WriteLine(deviceString);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
